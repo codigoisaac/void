f6f2d5d1585fa05a2ece2674a5b4bd7ccf459ba2
Isaac Muniz <isaacdmuniz@yahoo.com>
2020-08-08 09:05:52 -0300

    Choosing a random time

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 document.querySelector("#play-button").addEventListener("click", play);
 document.querySelector("#the-logo").addEventListener("click", voidit);
+const times = [...document.querySelectorAll(".time")];
 
 function play() {
-  console.log("play");
+  /* When I press the play button
+  without selecting any time, it chooses
+  randomly one of the time options */
+  times.forEach((time) => time.classList.remove("selected"));
+  const selectedTime = times[Math.floor(Math.random() * times.length)];
+  selectedTime.classList.add("selected");
 }
+
+function playTime() {}
 
 function voidit() {
   console.log("voidit");
 }
-
-// Cursor
-const cursor = document.querySelector("#cursor");
-const cursorSize = cursor.offsetWidth;
-const offset = cursorSize / 2;
-
-document.addEventListener("mousemove", (e) => {
-  cursor.setAttribute(
-    "style",
-    "top: " + (e.pageY - offset) + "px; left: " + (e.pageX - offset) + "px;"
-  );
-});
