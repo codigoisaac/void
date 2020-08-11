b1d7a7d8c8f54baba2b17f76f55e4d47ed440657
Isaac Muniz <isaacdmuniz@yahoo.com>
2020-08-11 10:22:49 -0300

    Random timer done
    But I'm not shure if random time choice is important to be in the app.

--- script.js
+++ script.js
@@ -1,60 +1,54 @@
-document.querySelector("#play-button").addEventListener("click", play);
-document.querySelector("#the-logo").addEventListener("click", voidit);
-const times = [...document.querySelectorAll(".time")];
+let countdown;
+const timerDisplay = document.querySelector("#timer-display");
+const timeOptions = [...document.querySelectorAll(".time")];
 
-let timerIntervalId = 0;
-let selectedTime = times[Math.floor(Math.random() * times.length)];
+const play = document.querySelector("#play-button");
+play.addEventListener("click", startTimer);
 
-// can't access object property on object initialization.
-// const time = {
-//   minutes: parseInt(selectedTime.dataset.time),
-//   // seconds: this.minutes * 60,
-//   seconds: 5,
-//   milliseconds: this.seconds * 1000,
-// };
+function startTimer() {
+  // unselect any previously selected time option
+  timeOptions.forEach((option) => {
+    option.classList.remove("selected");
+  });
 
-const time = new (function () {
-  this.minutes = parseInt(selectedTime.dataset.time);
-  // this.seconds = this.minutes * 60;
-  this.seconds = 3;
-  this.milliseconds = this.seconds * 1000;
-})();
+  // select a random time from time options
+  const selectedTime =
+    timeOptions[Math.floor(Math.random() * timeOptions.length)];
+  // initialize timer
+  const minutesSelected = parseInt(selectedTime.dataset.time);
+  timer(minutesSelected);
 
-// what if i want to create multiple 'time' objects?
-// const time = new Object();
-// time.minutes = parseInt(selectedTime.dataset.time);
-// time.seconds = time.minutes * 60;
-// time.milliseconds = time.seconds * 1000;
-
-// how will i reference a property of it?
-// function Time() {
-//   this.minutes = selectedTime.dataset.time;
-//   this.seconds = this.minutes * 60;
-//   this.milliseconds = this.seconds * 1000;
-// }
-
-function play() {
-  /* When I press the play button
-  without selecting any time, it chooses
-  randomly one of the time options */
-  // Select a random time / unselect all first
-  times.forEach((time) => time.classList.remove("selected"));
-  selectedTime = times[Math.floor(Math.random() * times.length)];
+  // change background of selected time option
   selectedTime.classList.add("selected");
-  /* And the timer starts */
-  // timerIntervalId = setInterval("displayTime()", 1000);
-  setTimeout("timeEnd()", time.milliseconds);
-  console.log(time.milliseconds);
 }
 
-function displayTime() {
-  console.log("one second passed");
+function timer(minutes) {
+  // clear any active timers
+  clearInterval(countdown);
+
+  // get now
+  const now = Date.now(); // value in milliseconds
+  // calculate when it will stop
+  const then = now + minutes * 60000; // converted to milliseconds
+
+  displayTimeLeft(minutes * 60); // converted to seconds
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // stop if reached zero
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display
+    displayTimeLeft(secondsLeft);
+  }, 1000);
 }
 
-function timeEnd() {
-  console.log("time ended");
-}
-
-function voidit() {
-  console.log("voidit");
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display =
+    minutes + ":" + (remainderSeconds < 10 ? "0" : "") + remainderSeconds;
+  timerDisplay.textContent = display;
 }
