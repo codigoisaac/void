895c10a887ecafc7e0bbf3be1dfe9aa5782c4234
Isaac Muniz <isaacdmuniz@yahoo.com>
2020-09-08 00:58:18 -0300

    Open/Close Popup + Popup Responsiveness

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,24 @@
-const popup = document.querySelector("#info-btn");
+const infoBtn = document.querySelector("#info-btn"), // to click
+  popup = document.querySelector("#popup"), // to be opened/closed
+  home = document.querySelector("#home"), // to insert 'open class'
+  hidable = document.querySelectorAll(".below-popup"); // to be hided/shown
 
-popup.addEventListener("click", openPopup);
+infoBtn.addEventListener("click", openPopup);
 
 function openPopup() {
-  if (!popup.classList.contains("open")) {
-    popup.classList.add("open");
+  if (!home.classList.contains("popup")) {
+    home.classList.add("popup");
+    hidable.forEach((element) => {
+      element.style.opacity = "0";
+      element.style.display = "none";
+    });
+    popup.style.display = "flex";
   } else {
-    popup.classList.remove("open");
+    home.classList.remove("popup");
+    hidable.forEach((element) => {
+      element.style.opacity = "1";
+      element.style.display = "flex";
+    });
+    popup.style.display = "none";
   }
 }
